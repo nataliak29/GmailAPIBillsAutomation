33d8b7f49b0851803604593deefefa37d74b081b
Natalia Kukushkina <natkk@amazon.com>
2020-09-21 16:08:44 -0400

    modified to get full path

--- connectToGmail.py
+++ connectToGmail.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import pickle
 import os.path
+import sys
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -15,8 +16,9 @@ def connectToGmail():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    
     if os.path.exists('credentials/token.pickle'):
-        with open('credentials/token.pickle', 'rb') as token:
+        with open(os.path.join(sys.path[0],'credentials/token.pickle', 'rb')) as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -24,10 +26,10 @@ def connectToGmail():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials/credentials.json', SCOPES)
+                os.path.join(sys.path[0],'credentials/credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('credentials/token.pickle', 'wb') as token:
+        with open(os.path.join(sys.path[0],'credentials/token.pickle', 'wb')) as token:
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
